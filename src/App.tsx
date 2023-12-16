42dfbff53d36d4c1aec2233828396eaef4ff73d5
Todu66 <giga.todua06@gmail.com>
2023-12-16 21:18:19 +0400

    installing react-router and implementing naviagtion through pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import axios from "axios";
-import CoinList,{CoinProps} from "./components/CoinList";
-
+import CoinList, { CoinProps } from "./components/CoinList";
+import CoinInfo from "./components/CoinInfo";
 
 function App() {
   const [coins, setCoins] = useState<CoinProps[]>([]);
-  
-  //const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkl';
 
   useEffect(() => {
     const url = "https://api.coingecko.com/api/v3/coins/markets";
@@ -22,11 +21,12 @@ function App() {
         },
       })
       .then((response) => {
+        console.log("API Response:", response.data);
         // Shuffle the response array to get a random selection of 10 coins
         const shuffledCoins = response.data
           .sort(() => Math.random() - 0.5)
           .slice(0, 10);
-          
+
         // Set the state with the randomly selected coins data
         setCoins(shuffledCoins);
       })
@@ -34,15 +34,18 @@ function App() {
         console.log(error);
         // Log any errors that occur during api request
       });
+    console.log("Coins data:", coins);
   }, []);
 
   return (
-    <div className="container flex flex-col ">
-      <div className="flex justify-center text-xl">
-        <h1 className="cursor-pointer">CoinGecko</h1>
-      </div>
-      {/* ---------- */}
-      <CoinList coins={coins}/>
+    <div className="container flex flex-col">
+      <Link to="/">
+        <h1 className="text-4xl font-bold text-center">CoinGecko</h1>
+      </Link>
+      <Routes>
+  <Route path="/" element={<CoinList coins={coins} />} />
+  <Route path="/coin/:id" element={<CoinInfo />} />
+</Routes>
     </div>
   );
 }
